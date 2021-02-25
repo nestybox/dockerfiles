eed48faa522a439742d86defb55b231b8782cf67
Cesar Talledo <ctalledo@nestybox.com>
2021-02-25 04:49:02 +0000

    Minor comment updates to the kindind Dockerfile.
    Signed-off-by: Cesar Talledo <ctalledo@nestybox.com>

--- kindind/build-kind.sh
+++ kindind/build-kind.sh
@@ -13,7 +13,7 @@ cp bin/kind /usr/local/bin
 # Remove the golang image used during the kind build (not needed anymore)
 docker image rm $(docker image ls -aq)
 
-# Get the nestybox/kindestnode:v1.18.2 image (temporarily needed for
+# Preload the nestybox/kindestnode:v1.18.2 image (temporarily needed for
 # the kind cluster nodes to bypass a bug in the OCI runc used inside
 # these nodes).
 docker pull nestybox/kindestnode:v1.18.2
