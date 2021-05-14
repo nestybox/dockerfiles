1137399657c3548ab2f3d2ec859a641ffc479f63
Rodny Molina <rmolina@nestybox.com>
2021-05-14 19:38:33 -0400

    Enhance kindind dockerfiles with latest suggestions
    Made the following changes:
    
    * Added instructions to build images.
    * Updated Kind instalation procedure to rely on latest stable release and not on the latest code.
    * Modified image pre-fetching script to account for Kind's new installation approach.
    * Adjusted kindestnode pointer to refer to registry.nestybox.com.
    
    Signed-off-by: Rodny Molina <rmolina@nestybox.com>

--- kindind/1.18/download-node-img.sh
+++ kindind/1.18/download-node-img.sh
@@ -4,16 +4,7 @@
 dockerd > /var/log/dockerd.log 2>&1 &
 sleep 3
 
-# build K8s.io KinD
-cd /home/admin/kind
-git pull
-make
-cp bin/kind /usr/local/bin
-
-# Remove the golang image used during the kind build (not needed anymore)
-docker image rm $(docker image ls -aq)
-
 # Preload the nestybox/kindestnode:v1.18.2 image (temporarily needed for
 # the kind cluster nodes to bypass a bug in the OCI runc used inside
 # these nodes).
-docker pull nestybox/kindestnode:v1.18.2
+docker pull registry.nestybox.com/nestybox/kindestnode:v1.18.2
---
+++ kindind/1.20/download-node-img.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# start dockerd (needed for building KinD)
+dockerd > /var/log/dockerd.log 2>&1 &
+sleep 3
+
+# Preload the nestybox/kindestnode:v1.20.2 image (temporarily needed for
+# the kind cluster nodes to bypass a bug in the OCI runc used inside
+# these nodes).
+docker pull registry.nestybox.com/nestybox/kindestnode:v1.20.2
